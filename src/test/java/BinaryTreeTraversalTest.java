7285ef7429ccd10b5adf75cabfeabc2f5c2dafed
Simon Winther <simonwinther22@gmail.com>
2022-05-30 22:20:12 +0200

    Initial commit

---
+++ src/test/java/BinaryTreeTraversalTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeTraversalTest {
+
+    @Test
+    void inOrderTraversal1() {
+        BinaryTreeTraversal sol = new BinaryTreeTraversal();
+        List<Integer> expected = List.of(1,3,2);
+
+        TreeNode three = new TreeNode(3, null, null);
+        TreeNode two = new TreeNode(2, three, null);
+        TreeNode root = new TreeNode(1, null, two);
+
+        assertEquals(expected, sol.inorderTraversal(root));
+    }
+
+    @Test
+    void inOrderTraversal2() {
+        BinaryTreeTraversal sol = new BinaryTreeTraversal();
+        List<Integer> expected = List.of();
+
+        TreeNode root = new TreeNode();
+        assertEquals(expected, sol.inorderTraversal(root));
+    }
+
+    @Test
+    void inOrderTraversal3() {
+        BinaryTreeTraversal sol = new BinaryTreeTraversal();
+        List<Integer> expected = List.of(1);
+
+        TreeNode root = new TreeNode(1);
+        assertEquals(expected, sol.inorderTraversal(root));
+    }
+}
---
+++ src/test/java/PathSumTest.java
@@ -0,0 +1,46 @@
+import com.sun.source.tree.Tree;
+import org.junit.jupiter.api.Test;
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PathSumTest {
+
+    @Test
+    void hasPathSumTest1() {
+        PathSum sol = new PathSum();
+
+
+        TreeNode seven = new TreeNode(7);
+        TreeNode two = new TreeNode(2);
+        TreeNode one = new TreeNode(1);
+        TreeNode eleven = new TreeNode(11, seven, two);
+        TreeNode thirteen = new TreeNode(13);
+        TreeNode four = new TreeNode(4, null, one);
+        TreeNode four_first = new TreeNode(4, eleven, null);
+        TreeNode eight = new TreeNode(8, thirteen, four);
+        TreeNode five = new TreeNode(5, four_first, eight);
+
+        assertTrue(sol.hasPathSum(five, 22));
+    }
+
+    @Test
+    void hasPathSumTest2() {
+        PathSum sol = new PathSum();
+
+        TreeNode three = new TreeNode(3);
+        TreeNode two = new TreeNode(2);
+        TreeNode one = new TreeNode(1, two, three);
+
+        assertFalse(sol.hasPathSum(one, 5));
+    }
+
+    @Test
+    void hasPathSumTest3() {
+        PathSum sol = new PathSum();
+
+        assertFalse(sol.hasPathSum(null, 0));
+    }
+}
